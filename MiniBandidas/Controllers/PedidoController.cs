35a868e699d232c26504aca1411a2a4c01d79a07
LuciusCR <lmauromcr@gmail.com>
2023-08-14 17:17:11 -0600

    Se agrega botón de pedido y controlador de pedido

---
+++ MiniBandidas/Controllers/PedidoController.cs
@@ -0,0 +1,20 @@
+﻿using MiniBandidas.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiniBandidas.Controllers
+{
+    public class PedidoController : Controller
+    {
+        // GET: Pedido
+        [autorizacionUsuario(idOperacion: (3))]
+
+        public ActionResult Pedido()
+        {
+            return View();
+        }
+    }
+}
