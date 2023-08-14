285f009f692c75f484ae559d815efacc78dae59e
JosselynQuesada <joselinquesadatinoco@gmail.com>
2023-08-14 14:53:46 -0600

    Botón Logout

---
+++ MiniBandidas/Controllers/LogoutController.cs
@@ -0,0 +1,21 @@
+﻿using MiniBandidas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace MiniBandidas.Controllers
+{
+        public class LogoutController : Controller
+        {
+            // GET: Logout
+            public ActionResult LogOff()
+            {
+                Session["Usuario"] = null;
+
+                return RedirectToAction("Index", "Login");
+            }
+        }
+}
